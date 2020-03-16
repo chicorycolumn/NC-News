b7304ebb6a2238f03a64794de7b6a7711bad2f0e
C <chrismatusemail@gmail.com>
2020-03-16 10:54:55 +0000

    Basic navbar visible.

---
+++ news/src/Frontpage.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Frontpage extends Component {
+  render() {
+    return (
+      <div>
+        All articles here
+        <br />
+        here
+        <br />
+        here
+        <br />
+        here
+        <br />
+        here
+        <br />
+        here
+        <br />
+        here
+        <br />
+        here
+      </div>
+    );
+  }
+}
+
+export default Frontpage;
